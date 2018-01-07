71e02d799d35b0e392f020ec44e44d732bbe2dc7
root <root@DESKTOP-N85T370.localdomain>
2018-01-07 16:06:43 -0500

    add jquery, qunit tests, list.js with keypress listeners

---
+++ lists/static/list.js
@@ -0,0 +1,13 @@
+var initialize = function () {
+  console.log('initialize called');
+  $('input[name="text"]').on('keypress', function (){
+    console.log('in keypress handler');
+    $('.has-error').hide();
+  })
+};
+console.log('list.js loaded');
+
+$('input[name="text"]').on('keypress', function () {
+  console.log('in keypress handler');
+  $('.has-error').hide();
+})
