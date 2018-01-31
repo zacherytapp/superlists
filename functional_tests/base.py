8f5de0f798486ce7dc7603f3e938c37783da4fbb
Zachery Tapp <zacherytapp@gmail.com>
2018-01-31 17:04:20 -0500

    add screenshot on failure to FT runner

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,5 +1,6 @@
 import unittest
 import time
+from datetime import datetime
 import os
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -12,6 +13,9 @@ from selenium.common.exceptions import WebDriverException
 from .server_tools import reset_database
 
 MAX_WAIT = 15
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -22,8 +26,41 @@ class FunctionalTest(StaticLiveServerTestCase):
             self.live_server_url = 'http://' + self.staging_server
             reset_database(self.staging_server)
 
+    def _test_has_failed(self):
+        return any(error for (method, error) in self._outcome.errors)
+
+     def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
+
     def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
